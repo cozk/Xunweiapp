7734a06ba32c24c9f884528df143a99238221861
Administrator <157584848@qq.com>
2017-10-24 16:01:05 +0800

    tabs1.11

--- xunwei/src/pages/tabs/tabs.ts
+++ xunwei/src/pages/tabs/tabs.ts
@@ -1,43 +1,23 @@
 import { Component,ViewChild  } from '@angular/core';
-
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { MePage } from '../me/me';
-import { Storage } from '@ionic/storage';
+
+
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
   tab4Root = MePage;
   @ViewChild('rootTabs') tabRef:any;
-  constructor(
-    private storage:Storage,
-  ) {
+  constructor() {
 
   }
   ionViewDidEnter() {
-    this.storage.ready().then(()=>{
-      this.storage.get('_page').then((val) => {
-        console.log(val);
-        if(!val){
-          this.tabRef.select(0);
-        }
-        if(val==1){
-          this.tabRef.select(1);
-        }
-        if(val==2){
-          this.tabRef.select(2);
-        }
-        if(val==3){
-          this.tabRef.select(3);
-        }
-      })
-    });
-
+    this.tabRef.select(0);
   }
 }
