e03f99df6f32a8514e22d84b5068040676bf38f3
dmowang <1206923412@qq.com>
2017-10-26 09:05:50 +0800

    菜谱列表10.25

--- xunwei/src/pages/cooklist/cooklist.ts
+++ xunwei/src/pages/cooklist/cooklist.ts
@@ -16,7 +16,34 @@ import {CooksearchPage} from "../cooksearch/cooksearch";
   templateUrl: 'cooklist.html',
 })
 export class CooklistPage {
-
+  list01=[
+    {'list01':'家常菜','val':'01'},
+    {'list01':'私家菜','val':'02'},
+    {'list01':'凉菜','val':'03'},
+    {'list01':'海鲜','val':'04'},
+  ];
+  list02=[
+    {'list02':'川菜','val':'01'},
+    {'list02':'湘菜','val':'02'},
+    {'list02':'粤菜','val':'03'},
+    {'list02':'东北菜','val':'04'},
+    {'list02':'鲁菜','val':'05'},
+    {'list02':'浙菜','val':'06'},
+    {'list02':'苏菜','val':'07'},
+  ];
+  list03=[
+    {'list03':'韩国料理','val':'01'},
+    {'list03':'日本料理','val':'02'},
+    {'list03':'法国菜','val':'03'},
+    {'list03':'意大利菜','val':'04'},
+    {'list03':'西餐面点','val':'06'},
+  ];
+  list04=[
+    {'list04':'蛋糕面包','val':'01'},
+    {'list04':'饼干配方','val':'02'},
+    {'list04':'甜品点心','val':'03'},
+  ];
+  cooklistModel: string='01' ;
   constructor(
     public navCtrl: NavController,
     public viewCtrl:ViewController,
