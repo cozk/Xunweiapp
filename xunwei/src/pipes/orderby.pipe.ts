b1c085669445a817f414afa7931dcc599d3cadd4
dmowang <1206923412@qq.com>
2017-10-27 10:47:29 +0800

    增加排序管道

---
+++ xunwei/src/pipes/orderby.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'orderBy'
+})
+export class OrderbyPipe implements PipeTransform {
+
+  transform(cooks: any, args?: any): any {
+    if(cooks && cooks.length) {
+      if (args == 2 && cooks) {
+        let new_cooks = cooks || [];
+        new_cooks.sort(function (a, b) {
+          return Date.parse(b.cktime) - Date.parse(a.cktime);//时间
+        });
+        return new_cooks;
+      }
+      else if (args == 1 && cooks) {
+        let new_cooks = cooks || [];
+        new_cooks.sort(function (a, b) {
+          return Date.parse(b.ckrenqi) - Date.parse(a.ckrenqi);//人气
+        });
+        return new_cooks;
+      }
+    }
+  }
+}
