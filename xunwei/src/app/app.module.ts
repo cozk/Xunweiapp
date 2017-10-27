db43d73dd50ad43ee4eeeb7f5d68ac5d13f52cf0
Administrator <157584848@qq.com>
2017-10-27 13:33:01 +0800

    10.27

--- xunwei/src/app/app.module.ts
+++ xunwei/src/app/app.module.ts
@@ -17,6 +17,10 @@ import { SettingPage } from '../pages/setting/setting';
 import { GuanzhuPage } from '../pages/guanzhu/guanzhu';
 import { FensiPage } from '../pages/fensi/fensi';
 import { EditPage } from '../pages/edit/edit';
+import { EditiconPage } from '../pages/editicon/editicon';
+import { EditorPage } from '../pages/editor/editor';
+import { EditpassPage } from '../pages/editpass/editpass';
+
 import { UpPage } from '../pages/up/up';
 import { LoginPage }from '../pages/login/login'
 import { RegisterPage }from '../pages/register/register'
@@ -64,6 +68,9 @@ import {SearchCooksPipe} from "../pipes/search-cooks.pipe"
     GuanzhuPage,
     FensiPage,
     EditPage,
+    EditpassPage,
+    EditorPage,
+    EditiconPage,
     UpPage,
     CaipuPage,
     PinglunPage,
