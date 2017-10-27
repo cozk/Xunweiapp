64f055aef15debdd5640e2836291813e45ea015a
dmowang <1206923412@qq.com>
2017-10-27 13:54:30 +0800

    shoucang10.27

---
+++ xunwei/src/pages/shoucang/shoucang.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ShoucangPage } from './shoucang';
+
+@NgModule({
+  declarations: [
+    ShoucangPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ShoucangPage),
+  ],
+})
+export class ShoucangPageModule {}
