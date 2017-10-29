b4a8017e26a44c554b94167db3bbc9452dd7fcd4
Administrator <157584848@qq.com>
2017-10-29 14:17:12 +0800

    10.27

---
+++ xunwei/src/pages/editpass/editpass.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EditpassPage } from './editpass';
+
+@NgModule({
+  declarations: [
+    EditpassPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EditpassPage),
+  ],
+})
+export class EditpassPageModule {}
